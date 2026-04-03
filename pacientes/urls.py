b00fdb0b0e13d712a838c82af56f803057b79cf5
helonogueira <heloisancc@al.insper.edu.br>
2026-04-03 10:06:11 -0300

    CRUD de pacientes com MongoDb Atlas

--- pacientes/urls.py
+++ pacientes/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('pacientes/', views.lista_pacientes, name='lista_pacientes'),
+    path('pacientes/cadastrar/', views.cadastrar_paciente, name='cadastrar_paciente'),
+    path('pacientes/editar/<str:id>/', views.editar_paciente, name='editar_paciente'),
+    path('pacientes/remover/<str:id>/', views.remover_paciente, name='remover_paciente'),
 ]
--- pacientes/views.py
+++ pacientes/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from bson import ObjectId
+from .db import pacientes_collection
 
-# Create your views here.
+@login_required
+def lista_pacientes(request):
+    pacientes = list(pacientes_collection.find())
+    for paciente in pacientes:
+        paciente['id'] = str(paciente['_id'])
+    return render(request, 'pacientes/lista.html', {'pacientes': pacientes})
+
+@login_required
+def cadastrar_paciente(request):
+    if request.method == 'POST':
+        paciente = {
+            'nome': request.POST['nome'],
+            'data_nascimento': request.POST['data_nascimento'],
+            'contato': request.POST['contato'],
+            'especialidade': request.POST['especialidade'],
+            'observacoes': request.POST.get('observacoes', ''),
+        }
+        pacientes_collection.insert_one(paciente)
+        messages.success(request, 'Paciente cadastrado com sucesso!')
+        return redirect('lista_pacientes')
+    return render(request, 'pacientes/form.html', {'acao': 'Cadastrar'})
+
+@login_required
+def editar_paciente(request, id):
+    paciente = pacientes_collection.find_one({'_id': ObjectId(id)})
+    if request.method == 'POST':
+        pacientes_collection.update_one(
+            {'_id': ObjectId(id)},
+            {'$set': {
+                'nome': request.POST['nome'],
+                'data_nascimento': request.POST['data_nascimento'],
+                'contato': request.POST['contato'],
+                'especialidade': request.POST['especialidade'],
+                'observacoes': request.POST.get('observacoes', ''),
+            }}
+        )
+        messages.success(request, 'Paciente atualizado com sucesso!')
+        return redirect('lista_pacientes')
+    return render(request, 'pacientes/form.html', {'acao': 'Editar', 'paciente': paciente})
+
+@login_required
+def remover_paciente(request, id):
+    paciente = pacientes_collection.find_one({'_id': ObjectId(id)})
+    if request.method == 'POST':
+        pacientes_collection.delete_one({'_id': ObjectId(id)})
+        messages.success(request, 'Paciente removido com sucesso!')
+        return redirect('lista_pacientes')
+    return render(request, 'pacientes/confirmar_remocao.html', {'paciente': paciente})
