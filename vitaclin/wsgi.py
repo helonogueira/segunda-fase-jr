b0874c77c9fd2362d7d59493cbbe70b43f089cc0
helonogueira <heloisancc@al.insper.edu.br>
2026-04-03 16:39:15 -0300

    cria usuário padrão automaticamente no startup - resolve perda de usuário a cada deploy

--- vitaclin/wsgi.py
+++ vitaclin/wsgi.py
@@ -2,4 +2,13 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vitaclin.settings')
+
 application = get_wsgi_application()
+
+# Cria usuário padrão automaticamente se não existir
+try:
+    from django.contrib.auth.models import User
+    if not User.objects.filter(username='vitaclin').exists():
+        User.objects.create_superuser('vitaclin', '', 'vitaclin2026')
+except Exception:
+    pass
