9d4d9f4062cde3cf21ef1220829b8ab83c9ddf60
helonogueira <heloisancc@al.insper.edu.br>
2026-04-03 14:20:24 -0300

    redirect da raiz para login - rota '/' agora redireciona automaticamente para a tela de autenticação

--- vitaclin/urls.py
+++ vitaclin/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('', lambda request: redirect('login'), name='home'),
     path('', include('pacientes.urls')),
 ]
