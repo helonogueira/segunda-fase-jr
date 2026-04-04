1137130094eb5feae4578bb2b20059fda2087bd8
helonogueira <heloisancc@al.insper.edu.br>
2026-04-04 13:43:33 -0300

    adiciona tlsInsecure para resolver SSL handshake no Render

--- pacientes/db.py
+++ pacientes/db.py
@@ -9,6 +9,7 @@ client = MongoClient(
     serverSelectionTimeoutMS=5000,
     connectTimeoutMS=5000,
     socketTimeoutMS=5000,
+    tlsInsecure=True,
 )
 db = client[os.getenv('MONGO_DB', 'vitaclin')]
 pacientes_collection = db['pacientes']
