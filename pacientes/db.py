ef3bccca545b8b925045e0686462d32e71da5163
helonogueira <heloisancc@al.insper.edu.br>
2026-04-04 13:53:36 -0300

    downgrade pymongo 3.12.3 para resolver incompatibilidade TLS no Render

--- pacientes/db.py
+++ pacientes/db.py
@@ -1,15 +1,9 @@
 import os
-import ssl
-import certifi
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
 load_dotenv()
 
-client = MongoClient(
-    os.getenv('MONGO_URI', 'mongodb://localhost:27017'),
-    serverSelectionTimeoutMS=5000,
-    tlsCAFile=certifi.where(),
-)
+client = MongoClient(os.getenv('MONGO_URI', 'mongodb://localhost:27017'))
 db = client[os.getenv('MONGO_DB', 'vitaclin')]
 pacientes_collection = db['pacientes']
